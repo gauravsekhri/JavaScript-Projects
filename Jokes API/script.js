4d43a01bc04c6d22e8644ef112cae07494e12de5
Gaurav Sekhri <gauravsekhri69@gmail.com>
2021-02-03 13:18:41 +0530

    Add files via upload

---
+++ Jokes API/script.js
@@ -0,0 +1,48 @@
+const jokes = document.getElementById('joke');
+const jokeBtn = document.querySelector('#jokeBtn');
+
+
+
+
+// const generateJokes = () => {
+
+//     const setHeader = {
+//         headers: {
+//             Accept : "application/json"
+//         }
+//     }
+
+//     fetch('https://icanhazdadjoke.com', setHeader)
+//     .then((res) => res.json() )
+//     .then((data) => {
+//         jokes.innerHTML = data.joke;
+//     }).catch((error) => {
+//         console.log(error);
+//     })
+
+// }
+
+
+// Asynchronous await Method with error handling
+
+const generateJokes = async () => {
+
+    try{
+        const setHeader = {
+            headers: {
+                Accept : "application/json"
+            }
+        }
+    
+        const res = await fetch('https://icanhazdadjoke.com', setHeader)
+        const data = await res.json();
+        jokes.innerHTML = data.joke;
+    }catch(err){
+        console.log(`error is ${err}`);
+    }
+
+}
+
+
+jokeBtn.addEventListener('click', generateJokes);
+generateJokes();
